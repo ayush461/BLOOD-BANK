3e7f8d19bb64075ebccb7875db25dac3d17dc504
ayush <ayushagarwal8190@gmail.com>
2022-05-16 15:18:23 +0530

    donor

--- public/js/donate.js
+++ public/js/donate.js
@@ -1,7 +1,7 @@
 var input = document.querySelector('input'); // get the input element
 input.addEventListener('input', resizeInput); // bind the "resizeInput" callback on "input" event
 resizeInput.call(input); // immediately call the function
-
+//donor details
 function resizeInput() {
   if (this.value > 5) {
     this.value = 5;
